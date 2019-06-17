99639d9c3413e9d156da8e92a00c4cc9a86a6eb4
Igor Vallim <igor.vallim@gmail.com>
2019-06-17 02:20:51 -0300

    Commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatTabsModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatButtonModule, MatListModule } from '@angular/material';
+import { MenuComponent } from './shared/menu/menu.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MenuLateralComponent } from './shared/menu-lateral/menu-lateral.component';
+import { HomeComponent } from './home/home.component';
+import { NoticiasComponent } from './noticias/noticias.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import {CarouselModule} from 'ngx-bootstrap/carousel'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MenuLateralComponent,
+    HomeComponent,
+    NoticiasComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule, 
+    MatButtonModule, 
+    FlexLayoutModule, 
+    MatListModule,
+    CarouselModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
